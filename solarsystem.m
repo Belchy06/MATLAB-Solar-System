045e800b56401793df26a215d58d46b41bbb8575
Belchy06 <william.belcher@hotmail.com>
2019-03-29 19:10:29 +1000

    Completed basic functionality

--- solarsystem.m
+++ solarsystem.m
@@ -26,12 +26,12 @@ end
 
 % Physical constants
 % Graph Variables
-pens = [animatedline('LineWidth', 2),animatedline('LineWidth', 2),animatedline('LineWidth', 2),animatedline('LineWidth', 2),animatedline('LineWidth', 2)];
+pens = [animatedline('LineWidth', 2, 'Color','y'),animatedline('LineWidth', 2, 'Color','b'),animatedline('LineWidth', 2, 'Color','magenta'),animatedline('LineWidth', 2, 'Color','g'),animatedline('LineWidth', 2, 'Color','r'),animatedline('LineWidth', 2),animatedline('LineWidth', 2),animatedline('LineWidth', 2),animatedline('LineWidth', 2)];
 set(gca,'XLim', [-1836100000000 1836100000000],'YLim', [-1836100000000 1836100000000],'ZLim', [-1836100000000 1836100000000]);  
- 
+
 
 % Time constants
-delta_t = 60 * 60 * 24;
+delta_t = 60 * 60;
 
 % Universal Gravitational Constant
 G = 6.673 * 10^-11;
@@ -41,51 +41,60 @@ G = 6.673 * 10^-11;
 % Write your code here
 % Loop through code for each second
 for t=1:stop_time
-   for planetA=1:length(p)
-        planetAPosition = [p(planetA,1), p(planetA,2)];
-        planetAVelocity = [v(planetA,1), v(planetA,2)];
+    
+    for planetA=1:length(p)
+    
+        % Retrieve data from supplied data
+        planetAPosition = [p(planetA,1), p(planetA,2), p(planetA,3)];
+        planetAVelocity = [v(planetA,1), v(planetA,2), v(planetA,3)];
         planetAMass = mass(planetA);
+
+        for planetB=(planetA+1):length(p)
         
-       for planetB=1:length(p) 
-            planetBPosition = [p(planetB,1), p(planetB,2)];
+            % Retrieve data from supplied data
+            planetBPosition = [p(planetB,1), p(planetB,2), p(planetB,3)];
             planetBMass = mass(planetB);
 
-            if planetA ~= planetB
-                differenceVector = planetBPosition - planetAPosition;
-                differenceMagnitude = norm(differenceVector);
-                
-                forceVector = G * ((planetAMass * planetBMass) / differenceMagnitude^3) .* differenceVector;
-                
-                % Gravity Vector from planet B -> A
-                % Positive value as specified in assginment sheet
-                gravityVectorArray.x(planetB,planetA) = forceVector(1);
-                gravityVectorArray.y(planetB,planetA) = forceVector(2);
+            % Calculate necessary values as defined in assignment sheet
+            distanceVector = planetBPosition - planetAPosition;
+            distanceVectorMagnitude = sqrt(distanceVector(1)^2 + distanceVector(2)^2 + distanceVector(3)^2);
 
-                % Gravity Vector from planet A -> B
-                % Negative value can be achieved by multiplying gravity vector component by -1
-                % as the direction is just reversed. No need to recalculate components
-                gravityVectorArray.x(planetA,planetB) = forceVector(1) * -1;
-                gravityVectorArray.y(planetA,planetB) = forceVector(2) * -1;
-                
-                
-                newAcceleration.x = (1 / planetAMass) * gravityVectorArray(:,planetA).x;
-                newAcceleration.y = (1 / planetAMass) * gravityVectorArray(:,planetA).y;
-                
-                newVelocity.x = planetAVelocity(1) + (newAcceleration.x * delta_t);
-                newVelocity.y = planetAVelocity(2) + (newAcceleration.y * delta_t);
-                
-                newPosition.x = (planetAVelocity(1) * delta_t) + (0.5 * newAcceleration.x * (delta_t^2));
-                newPosition.y = (planetAVelocity(2) * delta_t) + (0.5 * newAcceleration.y * (delta_t^2));
-                
-                p(planetA) = newPosition;
-                v(planetA) = newVelocity;
-                
-                
-                addpoints(pens(planetA), p(planetA,1), p(planetA,2), p(planetA,3));
-                drawnow(); 
-            end      
-       end
-   end
+            gravityVector = ((G * planetAMass * planetBMass) / abs(distanceVectorMagnitude^3)) * distanceVector;
+            
+            % Gravity Vector from planet B -> A
+            % Positive value as specified in assginment sheet
+            gravityVectorArray.x(planetB,planetA) = gravityVector(1);
+            gravityVectorArray.y(planetB,planetA) = gravityVector(2);
+            gravityVectorArray.z(planetB,planetA) = gravityVector(3);
+
+            % Gravity Vector from planet A -> B
+            % Negative value can be achieved by multiplying gravity vector component by -1
+            % as the direction is just reversed. No need to recalculate components
+            gravityVectorArray.x(planetA,planetB) = gravityVector(1) * -1;
+            gravityVectorArray.y(planetA,planetB) = gravityVector(2) * -1;
+            gravityVectorArray.z(planetA,planetB) = gravityVector(3) * -1;  
+
+        end
+
+        % Total Gravity in xyz components that affects planetA
+        totalGravity.x = sum(gravityVectorArray.x(:,planetA));
+        totalGravity.y = sum(gravityVectorArray.y(:,planetA));
+        totalGravity.z = sum(gravityVectorArray.z(:,planetA));
+        
+        % Move total gravity into an array of acceleration values
+        gravitationalAcceleration = [totalGravity.x/planetAMass totalGravity.y/planetAMass totalGravity.z/planetAMass];
+        newPlanetAVelocity = planetAVelocity + (delta_t * gravitationalAcceleration);
+        newPlanetAPosition = planetAPosition + (newPlanetAVelocity * delta_t) + (gravitationalAcceleration .* (0.5 * delta_t^2));
+
+        p(planetA,:) = newPlanetAPosition;
+        v(planetA,:) = newPlanetAVelocity;
+        % Draw positions of planet
+        addpoints(pens(planetA), p(planetA,1), p(planetA,2), p(planetA,3));
+        drawnow(); 
+    end
+    
+    
+    
 end
   
 
